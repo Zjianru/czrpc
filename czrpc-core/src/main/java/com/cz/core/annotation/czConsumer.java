d744a9016f71488a11fe2ae4fa4249cc415e8ffd
zjianru <864156480@qq.com>
2024-03-18 21:02:06 +0800

    add consume annotation

---
+++ czrpc-core/src/main/java/com/cz/core/annotation/czConsumer.java
@@ -0,0 +1,16 @@
+package com.cz.core.annotation;
+
+import java.lang.annotation.*;
+
+/**
+ * czrpc - annotation
+ * 将资源标记为消费者
+ *
+ * @author Zjianru
+ */
+@Inherited
+@Target({ElementType.FIELD})
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+public @interface czConsumer {
+}
