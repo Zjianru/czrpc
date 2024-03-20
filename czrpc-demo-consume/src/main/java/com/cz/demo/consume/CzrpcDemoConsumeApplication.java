b57f2ac3d057722d485ec74e9bff01372e07c024
zjianru <864156480@qq.com>
2024-03-20 18:01:58 +0800

    add consumer project

---
+++ czrpc-demo-consume/src/main/java/com/cz/demo/consume/CzrpcDemoConsumeApplication.java
@@ -0,0 +1,13 @@
+package com.cz.demo.consume;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CzrpcDemoConsumeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CzrpcDemoConsumeApplication.class, args);
+    }
+
+}
---
+++ czrpc-demo-consume/src/test/java/com/cz/demo/consume/CzrpcDemoConsumeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cz.demo.consume;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CzrpcDemoConsumeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
