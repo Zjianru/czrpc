0ba43e1ccb013fda873926a1d158fac1bf22748d
zjianru <864156480@qq.com>
2024-04-10 17:29:45 +0800

    test case enhance

--- czrpc-demo-provider/src/main/java/com/cz/demo/provider/ProviderApplication.java
+++ czrpc-demo-provider/src/main/java/com/cz/demo/provider/ProviderApplication.java
@@ -44,7 +44,7 @@ public class ProviderApplication {
         //  test 1 parameter method
         System.out.println("Provider Case 1. >>===[基本测试：1个参数]===");
         RpcRequest request = new RpcRequest();
-        request.setService("cn.kimmking.kkrpc.demo.api.UserService");
+//        request.setService("cn.kimmking.kkrpc.demo.api.UserService");
         request.setMethodSign("findById@1_int");
         request.setArgs(new Object[]{100});
 
@@ -54,7 +54,7 @@ public class ProviderApplication {
         // test 2 parameters method
         System.out.println("Provider Case 2. >>===[基本测试：2个参数]===");
         RpcRequest request1 = new RpcRequest();
-        request1.setService("cn.kimmking.kkrpc.demo.api.UserService");
+//        request1.setService("cn.kimmking.kkrpc.demo.api.UserService");
         request1.setMethodSign("findById@2_int_java.lang.String");
         request1.setArgs(new Object[]{100, "CC"});
 
@@ -64,7 +64,7 @@ public class ProviderApplication {
         // test 3 for List<User> method&parameter
         System.out.println("Provider Case 3. >>===[复杂测试：参数类型为List<User>]===");
         RpcRequest request3 = new RpcRequest();
-        request3.setService("cn.kimmking.kkrpc.demo.api.UserService");
+//        request3.setService("cn.kimmking.kkrpc.demo.api.UserService");
         request3.setMethodSign("getList@1_java.util.List");
         List<User> userList = new ArrayList<>();
         userList.add(new User(100, "KK100"));
@@ -76,7 +76,7 @@ public class ProviderApplication {
         // test 4 for Map<String, User> method&parameter
         System.out.println("Provider Case 4. >>===[复杂测试：参数类型为Map<String, User>]===");
         RpcRequest request4 = new RpcRequest();
-        request4.setService("cn.kimmking.kkrpc.demo.api.UserService");
+//        request4.setService("cn.kimmking.kkrpc.demo.api.UserService");
         request4.setMethodSign("getMap@1_java.util.Map");
         Map<String, User> userMap = new HashMap<>();
         userMap.put("P100", new User(100, "KK100"));
